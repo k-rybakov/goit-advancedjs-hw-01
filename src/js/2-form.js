dccb18372332ebd4a20f8e2b52fb3291b323babb
Konstantin Rybakov <k.ribakov@gmail.com>
2024-10-16 21:31:23 +0300

    first commit

---
+++ src/js/2-form.js
@@ -0,0 +1,37 @@
+const formData = { email: "", message: "" };
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+window.addEventListener('DOMContentLoaded', populateForm);
+
+form.addEventListener('input', e => {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+  formData.email = '';
+  formData.message = '';
+  form.reset();
+});
+
+function populateForm() {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  if (savedData) {
+    const parsedData = JSON.parse(savedData);
+    formData.email = parsedData.email || '';
+    formData.message = parsedData.message || '';
+    form.elements.email.value = formData.email;
+    form.elements.message.value = formData.message;
+  }
+}
